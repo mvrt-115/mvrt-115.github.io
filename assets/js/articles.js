1ff95e9440dfa8111a8dea2213c867d2856f9cd5
Jatin Soni <jatinsoni006@gmail.com>
2020-05-18 18:34:39 -0700

    updated mvrtlib to be on site

---
+++ assets/js/articles.js
@@ -0,0 +1,16 @@
+var articles=[  '<div id="ar1c0arend"class="gdocs"><iframe src="https://docs.google.com/document/d/e/2PACX-1vROs8jo8ACepxtUpzYKxQ8IFOIJOZggVsBreT7G9H4gVxQrzJwEJ5X-mVEY0t5014SWMizsbwAF8KBt/pub?embedded=true"></iframe></div>'+
+                  '<div class="vids"><iframe style="width:100%;height:185px;" src="https://www.youtube.com/embed/mrHiJTzKvvo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'+
+                  '<iframe style="width:100%;height:185px;" src="https://www.youtube.com/embed/V7kUrfJid68" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'+
+	                '<iframe style="width:100%;height:185px;" src="https://www.youtube.com/embed/lmo5PSY6ylo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'+
+	                '<iframe style="width:100%;height:185px;" src="https://www.youtube.com/embed/JjblK0f2EuM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'+
+				 	        '<iframe style="width:100%;height:185px;" src="https://www.youtube.com/embed/PHdBWPXwJTA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>',
+
+  	            '<div id="ar2c0arend"class="gdocs"><iframe src="https://docs.google.com/document/d/e/2PACX-1vTJR1meJliZqJ8qUwoMlry1MIw_kGuNSi7Qu9-WH184Q4khqU9QBpPD0aSz1mx3c_c_PfXl_QgcFsLa/pub?embedded=true"></iframe> </div>'+
+        					'<div class="vids"> <iframe style="width:100%;height:185px;" src="https://www.youtube.com/embed/Qvyas0NOB2o" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>',
+
+        				'<div id="ar3c0arend"class="gdocs"><iframe src="https://docs.google.com/document/d/e/2PACX-1vSs-slxrVTnd5mIt6lgCUwZGVPhXEa7R82LwrCvbGbaC8mhm0NjaFY5WRMM8gp7OaS1DKJ9TpZIVn7I/pub?embedded=true"></iframe></div>',
+
+        				'<div id="ar2c2arend"class="gdocs"><iframe src="https://docs.google.com/document/d/e/2PACX-1vT2HvA9Jt0rWJ2qa3thV63yr-3P3Pc-_vb6VRmxpHKrHuMCsU5mm4_BWpNJ7AY8jAcwwam4Pt5fc94T/pub?embedded=true"></iframe></div>',
+
+        				'<div id="ar1c2arend"class="gdocs"><iframe src="https://docs.google.com/document/d/e/2PACX-1vQjgnuI71NUnnlz3ydwnu-1om63NkbaCkxtibOPeh4I2CcCaSTgIyeRngA-GHcea1bowUcEExtqdpfg/pub?embedded=true"></iframe></div>'+
+        				  	'<div class="vids"> <iframe style="width:100%;height:185px;" src="https://www.youtube.com/embed/V7kUrfJid68" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>'];
