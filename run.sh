7f6fd0073fd5dc9ef36af3eb8ad5389aa76404c0
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-06-09 14:33:21 -0700

    Changed team image for 2022 + removed old .xcf file for gimp

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# (Lilja) This script is just for ease of opening website in browser for debugging purposes with Linux [will delete later]
+
+#Open temp website
+google-chrome index.html &
+
+#Open official website in new tab for comparison
+google-chrome mvrt.com
